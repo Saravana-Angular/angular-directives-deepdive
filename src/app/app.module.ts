8c75c85f512e76a32abaa7c75049aac0b50ef040
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-05-30 11:08:05 -0400

    implemented HostListener decorator to list mouseover to change the style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BasicHighlight } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlight
+    BasicHighlight,
+    BetterHighlightDirective
+    
   ],
   imports: [
     BrowserModule,
---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit{
+
+  constructor(private elRef: ElementRef,private renderer: Renderer2) { }
+
+  ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'green');
+  }
+
+  @HostListener('mouseenter') mouseover() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'green');
+  }
+
+  @HostListener('mouseleave') mouseleave() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+  }
+
+}
